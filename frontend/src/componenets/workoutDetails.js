334c610f28fe0a303ffe8f4d135f29e66c838fc8
Sam Gardner <samgardner20@gmail.com>
2022-12-19 15:58:25 -0500

    added in a delete button that deleted from db

--- frontend/src/componenets/workoutDetails.js
+++ frontend/src/componenets/workoutDetails.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import { useWorkoutContext } from '../hooks/useWorkoutContext';
 
 const WorkoutDetails = ({workout}) => {
+
+  const { dispatch } = useWorkoutContext()
+
+  const handleClick = async () => {
+
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+
+    const json = await response.json()
+
+    if (response.ok){
+      console.log("deleted");
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
+  }
+
+
     return (
       <div className="workout-details">
         <h4>{workout.title}</h4>
@@ -17,6 +36,7 @@ const WorkoutDetails = ({workout}) => {
           {workout.sets}
         </p>
         <p>{workout.createdAt}</p>
+        <span onClick={handleClick}>Delete</span>
       </div>
     );
 }
--- frontend/src/context/workouts.context.js
+++ frontend/src/context/workouts.context.js
@@ -3,17 +3,25 @@ import { createContext, useReducer } from "react";
 export const WorkoutContext = createContext()
 
 export const workoutReducer = (state, action) => {
-    switch(action.type){
-        case 'SET_WORKOUTS':
-            return {
-                workouts: action.payload
-            }
-        case 'CREATE_WORKOUT':
-            return {
-                workouts: [action.payload, ...state.workouts]
-            }
-        default:
-            return state
+    switch (action.type) {
+      case 'SET_WORKOUTS':
+        return {
+          workouts: action.payload
+        };
+      case 'CREATE_WORKOUT':
+        return {
+          workouts: [action.payload, ...state.workouts]
+        };
+      case 'DELETE_WORKOUT':
+        return {
+          workouts: state.workouts.filter(w => w._id !== action.payload._id)
+        };
+    //   case 'UPDATE_WORKOUT':
+    //     return {
+    //       workouts: [action.payload, ...state.workouts]
+    //     };
+      default:
+        return state;
     }
 }
 
