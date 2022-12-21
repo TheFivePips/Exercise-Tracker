8eb97293d4d83b3c594230b1802d5c9140641005
Sam Gardner <samgardner20@gmail.com>
2022-12-21 12:43:24 -0500

    added in a auth context for the front end to start using

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,11 @@ const app = express()
 // middleware
 app.use(express.json())
 
-app.use((req, res, next) =>{
-    console.log(req.path, req.method);
-    next()
+// app.use((req, res, next) =>{
+//     console.log(req.path, req.method);
+//     next()
 
-})
+// })
 
 
 // routes
